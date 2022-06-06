0cfe0b49c816469c07e5621b986d8a167f81084c
DubdeeB <ligolas@gmail.com>
2022-06-06 21:23:32 +0300

    Solved not in a very elegant solution the algo problem, and now have a naive implementation which was debuged through a simple example in main

--- Dijkstra_naive_implementation.py
+++ Dijkstra_naive_implementation.py
@@ -6,34 +6,32 @@ import numpy as np
 
 
 # PriorityQueue naive implementation copied from: https://www.geeksforgeeks.org/priority-queue-in-python/.
-# Adapted to have both index and priority
+# Adapted to have priorities in the poping function
 class PriorityQueue(object):
     def __init__(self):
         self.data = []
-        self.priority = []
 
     def __str__(self):
-        return ' '.join([str(self.data[i]) + ',' + str(self.priority[i]) + '; ' for i in range(len(self.data))])
+        return ' '.join([str(i) + ', ' for i in self.data])
 
     # for checking if the queue is empty
     def isEmpty(self):
         return len(self.data) == 0
 
     # for inserting an element in the queue
-    def insert(self, data, priority):
+    def insert(self, data):
         self.data.append(data)
-        self.priority.append(priority)
 
     # for popping an element based on Priority
-    def pop(self):
+    # I decided to give priorities as an input because one might wish to update it while poping
+    def pop(self, priorities):
         try:
             ind_max_val = 0
-            for i in range(len(self.data)):
-                if self.priority[i] > self.priority[ind_max_val]:
+            for i, p in enumerate(priorities):
+                if p > priorities[ind_max_val]:
                     ind_max_val = i
             item = self.data[ind_max_val]
             del self.data[ind_max_val]
-            del self.priority[ind_max_val]
             return item
         except IndexError:
             print()
@@ -57,14 +55,29 @@ def Dijkstra(lil_graph, weights_matrix, s, t):
     previous = [-1 for _ in range(n)]
     remaining = PriorityQueue()
     for node in range(n):
-        remaining.insert(node, path_weight[node])
+        remaining.insert(node)
 
     while not remaining.isEmpty():
-        node = remaining.pop()
+        node = remaining.pop([-path_weight[d] for d in remaining.data])
         for neighbor in lil_graph[node]:
             weight_via_node = path_weight[node] + weights_matrix[node, neighbor]
             if path_weight[neighbor] > weight_via_node:
                 path_weight[neighbor] = weight_via_node
                 previous[neighbor] = node
-    # Big bug! update path_weight vector does not update remaining priorities. Not sure how to fix it yet.
+    path_from_t_to_s = []
+    node = t
+    while node != s:
+        path_from_t_to_s.append(node)
+        node = previous[node]
+    path_from_t_to_s.append(s)
+    path_from_t_to_s.reverse()
+    return path_from_t_to_s
 
+
+def main():
+    lil_graph = [[1], [0]]
+    weights = np.array([[-1, 1], [1, -1]])
+    print(Dijkstra(lil_graph, weights, 0, 1))
+
+if __name__ == '__main__':
+    main()
